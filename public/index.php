5f2b2316f51be436cb844f2e6cf6c6a462183755
jai-kj <jai.j@somaiya.edu>
2024-04-04 18:52:07 +0530

    fix(server): added options methods handler and changed cors policy

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
 // Headers
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
+
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/Database.php';
