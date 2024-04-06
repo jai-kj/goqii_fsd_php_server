9b15fd5cc23b63c63f1c43ff701a998516714ba3
jai-kj <jai.j@somaiya.edu>
2024-04-06 16:46:32 +0530

    fix(index): allow cors from all endpoints

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Headers
-header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
